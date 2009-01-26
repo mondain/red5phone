b426e22dbdce819abd700064a840a709de08d806
seba.wagner <seba.wagner@b690c786-d04d-0410-b125-9d66a4221e52>
2009-01-26 13:23:06 +0000

    Fixes: Remove Dist Folder From Version Control

--- branches/webapp_rework/dist/sip/WEB-INF/classes/do_sip_compile.cmd
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Java\jdk1.6.0_01\bin\javac" -Xlint  -classpath .;"..\lib\vt4f.jar";..\lib\sip.jar;..\lib\ua.jar;"C:\Program Files\Red5\red5.jar";"C:\Program Files\Red5\lib\commons-lang-2.3.jar";"C:\Program Files\Red5\lib\spring-web-2.0.8.jar";"C:\Program Files\Red5\lib\spring-context-2.0.8.jar";"C:\Program Files\Red5\lib\spring-support-2.0.8.jar";"C:\Program Files\Red5\lib\spring-beans-2.0.8.jar";"C:\Program Files\Red5\lib\spring-aop-2.0.8.jar";"C:\Program Files\Red5\lib\spring-core-2.0.8.jar";"C:\Program Files\Red5\lib\mina-core-1.1.6.jar";"C:\Program Files\Red5\lib\mina-integration-jmx-1.1.6.jar"; org\red5\server\webapp\sip\*.java
-
-pause
