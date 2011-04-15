8ff6d0e2e0369bebc26c51a2fb79a141c6692ac8
olajide.dele <olajide.dele@b690c786-d04d-0410-b125-9d66a4221e52>
2011-04-15 10:34:37 +0000

    Support for Flash Player 10.3 wirh Echo cancellation

--- sip/flex/src/make_red5phone.cmd
+++ sip/flex/src/make_red5phone.cmd
@@ -1,4 +1,4 @@
-C:\Projects\flex33\bin\mxmlc.exe  red5phone.mxml
+C:\Projects\flex45\bin\mxmlc.exe  red5phone.mxml  -theme C:\Projects\flex45\frameworks\themes\Halo\halo.swc -swf-version=12
 
 copy red5phone.swf ..\red5phone.swf
 
