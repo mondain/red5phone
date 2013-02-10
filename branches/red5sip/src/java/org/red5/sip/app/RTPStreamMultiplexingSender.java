89639d478fa21e693af1ea0a97ccc837f816661d
timur.tleukenov@gmail.com <timur.tleukenov@gmail.com@b690c786-d04d-0410-b125-9d66a4221e52>
2013-02-10 20:42:20 +0000

    Support custom ASAO sampling rate; Resampling pcm to SIP codec sampling rate; Fixing bugs.

--- branches/red5sip/src/java/org/red5/sip/app/RTPStreamMultiplexingSender.java
+++ branches/red5sip/src/java/org/red5/sip/app/RTPStreamMultiplexingSender.java
@@ -1,5 +1,6 @@
 package org.red5.sip.app;
 
+import com.laszlosystems.libresample4j.Resampler;
 import local.net.RtpPacket;
 import local.net.RtpSocket;
 import org.apache.mina.util.ConcurrentHashSet;
@@ -20,9 +21,36 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
 
     public static int RTP_HEADER_SIZE = 12;
 
-    protected static final int NELLYMOSER_DECODED_PACKET_SIZE = 256;
+    public static enum SAMPLE_RATE {
+        SAMPLING_8000(8000, 8),
+        SAMPLING_16000(16000, 16),
+        SAMPLING_32000(32000, 32),
+        SAMPLING_48000(48000, 48),
+        SAMPLING_11025(11025, 11),
+        SAMPLING_22050(22050, 22),
+        SAMPLING_44100(44100, 44);
 
-    protected static final int NELLYMOSER_ENCODED_PACKET_SIZE = 64;
+        public int rate;
+        public int shortName;
+        SAMPLE_RATE(int rate, int shortName) {
+            this.rate = rate;
+            this.shortName = shortName;
+        }
+
+        public static SAMPLE_RATE findByShortName(int shortName) {
+            for(SAMPLE_RATE s: SAMPLE_RATE.values()) {
+                if(s.shortName == shortName) {
+                    return s;
+                }
+            }
+            throw new IllegalArgumentException("Invalid sample rate");
+        }
+    }
+
+    protected static SAMPLE_RATE sampling = SAMPLE_RATE.SAMPLING_22050;
+
+    protected static final int NELLYMOSER_DECODED_PACKET_SIZE = 256;// * sampling.blocks;
+    protected static final int NELLYMOSER_ENCODED_PACKET_SIZE = 64;// * sampling.blocks;
 
     RtpSocket rtpSocket = null;
 
@@ -46,25 +74,18 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
     private int seqn = 0;
 
     private long time = 0;
-    private long syncSource = 0;
 
     // Temporary buffer with received PCM audio from FlashPlayer.
-    float[] tempBuffer;
+    private float[] tempBuffer;
+    private float[] multiplexedBuffer;
 
-    float[] decodedBuffer1;
-    float[] decodedBuffer2;
-
-    volatile int multiplexingCount = 0;
+    private Resampler resampler;
+    private double factor = 1;
 
     private Thread sendThread = new Thread(this);
 
     ConcurrentHashSet<WeakReference<RTPStreamForMultiplex>> streamSet = new ConcurrentHashSet<WeakReference<RTPStreamForMultiplex>>();
     //Set<RTPStreamForMultiplex> streamSet = Collections.synchronizedSet(new WeakHashSet<RTPStreamForMultiplex>());
-
-    private static int SEND_BUFFER_MAX = 100;
-    private byte[][] sendBuffer = new byte[SEND_BUFFER_MAX][];
-    private int[] sendBufferLength = new int[SEND_BUFFER_MAX];
-    private int sendBufferPos = 0;
 
     // Floats remaining on temporary buffer.
     int tempBufferRemaining = 0;
@@ -168,11 +189,6 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
         mediaReceiver.setSender(this);
         this.sipCodec = sipCodec;
         this.doSync = do_sync;
-        this.sendBufferPos = 0;
-
-        for(int i=0; i<SEND_BUFFER_MAX; i++) {
-            sendBuffer[i] = new byte[sipCodec.getOutgoingEncodedFrameSize() + RTP_HEADER_SIZE];
-        }
 
         try {
             if (src_socket == null) {
@@ -194,24 +210,22 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
 
         seqn = 0;
         time = 0;
-        syncSource = 0;
 
         println("start()", "using blocks of " + (packetBuffer.length - RTP_HEADER_SIZE) + " bytes.");
 
         decoder = new Decoder();
-        decoderMap = null;
 
         sendThread.start();
     }
 
     private void fillDecodedBuffer(byte[] asaoBuffer, float[] tempBuffer) {
         ByteStream audioStream = new ByteStream(asaoBuffer, 1, NELLYMOSER_ENCODED_PACKET_SIZE);
-        decoderMap = decoder.decode(decoderMap, audioStream.bytes, 1, tempBuffer, 0);
+        decoderMap = decoder.decode(decoderMap, audioStream.bytes, 0, tempBuffer, 0);
         //ResampleUtils.normalize(tempBuffer, tempBuffer.length);
     }
 
-    public synchronized IMediaStream createStream(int streamId) {
-        RTPStreamForMultiplex stream = new RTPStreamForMultiplex(streamId, syncSource, this);
+    public IMediaStream createStream(int streamId) {
+        RTPStreamForMultiplex stream = new RTPStreamForMultiplex(streamId);
         streamSet.add(new WeakReference<RTPStreamForMultiplex>(stream));
         return stream;
     }
@@ -219,15 +233,30 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
     public void deleteStream(int streamId) {
         for (Iterator<WeakReference<RTPStreamForMultiplex>> iterator = streamSet.iterator(); iterator.hasNext(); ) {
             WeakReference<RTPStreamForMultiplex> ref = iterator.next();
-            if (ref.get() != null && ref.get().getStreamId() == streamId ) {
-                iterator.remove();
+            RTPStreamForMultiplex stream = ref.get();
+            try {
+                if (stream != null && stream.getStreamId() == streamId ) {
+                    iterator.remove();
+                }
+            } catch (NullPointerException ignored) {
+
             }
         }
     }
 
-    private void doRtpDelay() {
+    private void doRtpDelay(float bufferUsage) {
+        //TODO: make proper pause correction.
+        //Pause should not exceed packetization and prevent emtying of buffer
         try {
-            Thread.sleep( sipCodec.getOutgoingPacketization() - 2);
+            long pause = sipCodec.getOutgoingPacketization() - 1;
+            if(bufferUsage > .5f) {
+                pause -= 5;
+            }
+            if(bufferUsage > .2f) {
+                pause -= 1;
+            }
+            System.out.println("Sleep pause: " + pause);
+            Thread.sleep( pause, 800000 );
         }
         catch ( Exception e ) {
         }
@@ -235,48 +264,65 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
 
     public void run() {
         if (!hasInitilializedBuffers) {
-            tempBuffer = new float[NELLYMOSER_DECODED_PACKET_SIZE];
-            decodedBuffer1 = new float[NELLYMOSER_DECODED_PACKET_SIZE];
-            decodedBuffer2 = new float[NELLYMOSER_DECODED_PACKET_SIZE];
+            multiplexedBuffer = new float[NELLYMOSER_DECODED_PACKET_SIZE];
             encodingBuffer = new float[sipCodec.getOutgoingDecodedFrameSize()];
-            multiplexingCount = 0;
+
+            if(sipCodec.getSampleRate() == sampling.rate) {
+                tempBuffer = new float[NELLYMOSER_DECODED_PACKET_SIZE];
+                resampler = null;
+            } else {
+                factor = sipCodec.getSampleRate() / (double)sampling.rate;
+                resampler = new Resampler(true, factor, factor);
+                tempBuffer = new float[(int)(NELLYMOSER_DECODED_PACKET_SIZE * factor)];
+            }
             hasInitilializedBuffers = true;
         }
-        byte[] asaoBuffer = new byte[65];
+
+        float[] decodedBuffer = new float[NELLYMOSER_DECODED_PACKET_SIZE];
+        byte[] asaoBuffer = new byte[NELLYMOSER_ENCODED_PACKET_SIZE];
+
+        int disableStream = 0;
+
         while(rtpSocket != null) {
-            multiplexingCount = 0;
-            for(Iterator<WeakReference<RTPStreamForMultiplex>> i = streamSet.iterator(); i.hasNext();) {
-                int len = -1;
-                WeakReference<RTPStreamForMultiplex> ref = i.next();
-                RTPStreamForMultiplex stream = ref.get();
-                if(stream != null) {
-                    synchronized (stream) {
-                        System.out.println(String.format("Stream id %d, avail %d", stream.getStreamId(), stream.available()));
-                        if(stream != null && stream.ready()) {
+            float bufferUsage = 0;
+            int multiplexingCount = 0;
+            try {
+                for(Iterator<WeakReference<RTPStreamForMultiplex>> i = streamSet.iterator(); i.hasNext();) {
+                    int len = -1;
+                    WeakReference<RTPStreamForMultiplex> ref = i.next();
+                    RTPStreamForMultiplex stream = ref.get();
+                    if(stream != null) {
+                        if(stream.ready() && disableStream != stream.getStreamId()) {
                             len = stream.read(asaoBuffer, 0);
+                            bufferUsage = Math.max(bufferUsage, stream.bufferUsage());
+                            System.out.println(String.format("Stream id %d, buffer %f", stream.getStreamId(), stream.bufferUsage()));
                         } else {
-                            break;
+                            continue;
                         }
-                    }
-                } else {
-                    i.remove();
-                    break;
-                }
-                if(len != -1) {
-                    fillDecodedBuffer(asaoBuffer, decodedBuffer1);
-                    if (multiplexingCount > 0) {
-                        ResampleUtils.multiplex(decodedBuffer2, decodedBuffer1);
                     } else {
-                        BufferUtils.floatBufferIndexedCopy(decodedBuffer2, 0, decodedBuffer1, 0, decodedBuffer1.length);
+                        i.remove();
+                        continue;
                     }
-                    multiplexingCount++;
-                    //System.out.println("Multiplex stream: " + streamId);
+                    if(len != -1) {
+                        ByteStream audioStream = new ByteStream(asaoBuffer, 1, NELLYMOSER_ENCODED_PACKET_SIZE);
+                        stream.decoderMap = decoder.decode(stream.decoderMap, audioStream.bytes, 0, decodedBuffer, 0);
+                        //fillDecodedBuffer(asaoBuffer, decodedBuffer);
+                        if (multiplexingCount > 0) {
+                            ResampleUtils.multiplex(multiplexedBuffer, decodedBuffer);
+                        } else {
+                            System.arraycopy(decodedBuffer, 0, multiplexedBuffer, 0, decodedBuffer.length);
+                        }
+                        multiplexingCount++;
+                    }
+                    Thread.yield();
                 }
-                Thread.yield();
+            } catch (Exception e) {
+                log.error("Exception", e);
             }
             if(multiplexingCount > 0) {
-                //System.out.println("Send: multiplexed: " + multiplexingCount + ", total streams: " + streamSet.size());
-                ResampleUtils.normalize(decodedBuffer2, decodedBuffer2.length);
+                System.out.println("Send: multiplexed: " + multiplexingCount + ", total streams: " + streamSet.size());
+//                ResampleUtils.normalize(multiplexedBuffer, 1.0f/multiplexingCount);
+                ResampleUtils.normalize(multiplexedBuffer, multiplexedBuffer.length);
                 try {
                     asao_buffer_processed = false;
                     do {
@@ -286,7 +332,8 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
                         }
                         if (encodingOffset == sipCodec.getOutgoingDecodedFrameSize()) {
                             rtpSocketSend(rtpPacket);
-                            doRtpDelay();
+                            doRtpDelay(bufferUsage);
+                            //System.out.println("rtpSocketSend, bufferUsage: " + bufferUsage);
                             encodingOffset = 0;
                         }
                     }
@@ -339,10 +386,13 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
 
                 }
 
-                // Decode new asao packet.
-
+                // Process next buffer
                 asao_buffer_processed = true;
-                BufferUtils.floatBufferIndexedCopy(tempBuffer, 0, decodedBuffer2, 0, decodedBuffer2.length);
+                if(resampler == null) {
+                    System.arraycopy(multiplexedBuffer, 0, tempBuffer, 0, tempBuffer.length);
+                } else {
+                    resampler.process(factor, multiplexedBuffer, 0, multiplexedBuffer.length, true, tempBuffer, 0, tempBuffer.length);
+                }
 //                tempBuffer = ResampleUtils.normalize(tempBuffer, 256); 	// normalise volume
 
                 tempBufferRemaining = tempBuffer.length;
@@ -404,8 +454,6 @@ public class RTPStreamMultiplexingSender implements IMediaSender, Runnable {
 
         println("halt", "Terminated");
     }
-
-    private long lastMS = System.currentTimeMillis();
 
     private void rtpSocketSend(RtpPacket rtpPacket) {
         try {
