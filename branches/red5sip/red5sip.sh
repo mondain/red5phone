a2a8fdb5a6284e2c87a8b11728d0d4cf2326feea
solomax666@gmail.com <solomax666@gmail.com@b690c786-d04d-0410-b125-9d66a4221e52>
2013-02-12 10:46:43 +0000

    Auto-start script for red5sip is separated from red5sip.sh; jsvc dependancy is removed

--- branches/red5sip/red5sip.sh
+++ branches/red5sip/red5sip.sh
@@ -1,53 +1,16 @@
 #!/bin/sh
-### BEGIN INIT INFO
-# Provides:          red5sip
-# Required-Start:    networking
-# Required-Stop:
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-### END INIT INFO
+
+if [ -z "$RED5SIP_HOME" ]; then
+  export RED5SIP_HOME=`pwd`;
+fi
+
 
 JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
-echo $JAVA_HOME
-INSTALL_DIR=$(pwd)
-LIB_DIR=$INSTALL_DIR/lib
-LOGS_DIR=$INSTALL_DIR/log
-JSVC_ERR=$LOGS_DIR/jsvc_red5sip.err
-PID_FILE=/var/run/red5sip.pid
-SETTINGS_FILE=$INSTALL_DIR/settings.properties
+LIB_DIR=$RED5SIP_HOME/lib
+LOGS_DIR=$RED5SIP_HOME/log
+SETTINGS_FILE=$RED5SIP_HOME/settings.properties
 CLASSPATH=$(echo $LIB_DIR/*.jar | sed 's/ /:/g')
-case "$1" in
-  start)
-    #
-    # Start backend
-    #
-    echo "Starting red5sip service"
-    jsvc -pidfile $PID_FILE \
-        -home $JAVA_HOME \
-        -errfile $JSVC_ERR \
-        -Dlogs=$LOGS_DIR \
-        -cp $CLASSPATH org.red5.sip.app.Application $SETTINGS_FILE
-    ;;
 
-
-  stop)
-    #
-    # Stop red5sip service
-    #
-    jsvc -stop -pidfile $PID_FILE org.red5.sip.app.Application
-    exit $?
-    ;;
-  
-  run)
-    #
-    # Run red5sip
-    #
-    $JAVA_HOME/bin/java -Dlogs=$LOGS_DIR -cp $CLASSPATH org.red5.sip.app.Main $SETTINGS_FILE
-    ;;
-
-  *)
-    echo "Usage red5sip.sh start/stop"
-    exit 1;;
-esac
-
+echo "Starting Red5SIP"
+exec $JAVA_HOME/bin/java -Dlogs=$LOGS_DIR -cp $CLASSPATH org.red5.sip.app.Main $SETTINGS_FILE
 
