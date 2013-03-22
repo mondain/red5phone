c2284544ae3481eab3fd76933f8afc4b0d76b549
solomax666@gmail.com <solomax666@gmail.com@b690c786-d04d-0410-b125-9d66a4221e52>
2013-03-22 09:53:16 +0000

    "self made" basic classes Log, HashSet, Iterator are replaces by well known versions

--- branches/red5sip/src/java/org/zoolu/sip/transaction/AckTransactionClient.java
+++ branches/red5sip/src/java/org/zoolu/sip/transaction/AckTransactionClient.java
@@ -23,57 +23,45 @@
 
 package org.zoolu.sip.transaction;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zoolu.sip.message.Message;
 import org.zoolu.sip.provider.SipProvider;
-import org.zoolu.sip.provider.SipStack;
-import org.zoolu.tools.LogLevel;
 
+/**
+ * ACK client transaction should follow an INVITE client transaction within an INVITE Dialog in a SIP UAC. The
+ * AckTransactionClient simply sends an ACK request message and terminates.
+ */
+public class AckTransactionClient extends Transaction {
+	private static final Logger log = LoggerFactory.getLogger(AckTransactionClient.class);
 
+	/** the TransactionClientListener that captures the events fired by the AckTransactionClient */
+	TransactionClientListener transaction_listener;
 
-/** ACK client transaction should follow an INVITE client transaction within an INVITE Dialog in a SIP UAC.
-  * The AckTransactionClient simply sends an ACK request message and terminates.
-  */ 
-public class AckTransactionClient extends Transaction
-{  
+	/** Creates a new AckTransactionClient. */
+	public AckTransactionClient(SipProvider sip_provider, Message ack, TransactionClientListener listener) {
+		super(sip_provider);
+		request = new Message(ack);
+		transaction_listener = listener;
+		transaction_id = request.getTransactionId();
+		log.debug("id: " + String.valueOf(transaction_id));
+		log.debug("created");
+	}
 
-   /** the TransactionClientListener that captures the events fired by the AckTransactionClient */
-   TransactionClientListener transaction_listener;
+	/** Starts the AckTransactionClient and sends the ACK request. */
+	public void request() {
+		log.trace("start");
+		sip_provider.sendMessage(request);
+		changeStatus(STATE_TERMINATED);
+		// if (transaction_listener!=null) transaction_listener.onAckCltTerminated(this);
+		// (CHANGE-040421) free the link to transaction_listener
+		transaction_listener = null;
+	}
 
-
-   /** Creates a new AckTransactionClient. */
-   public AckTransactionClient(SipProvider sip_provider, Message ack, TransactionClientListener listener)
-   {  super(sip_provider);
-      request=new Message(ack);
-      transaction_listener=listener;
-      transaction_id=request.getTransactionId();
-      printLog("id: "+String.valueOf(transaction_id),LogLevel.HIGH);
-      printLog("created",LogLevel.HIGH);
-   }
-   
-   /** Starts the AckTransactionClient and sends the ACK request. */
-   public void request()
-   {  printLog("start",LogLevel.LOW);
-      sip_provider.sendMessage(request);
-      changeStatus(STATE_TERMINATED);  
-      //if (transaction_listener!=null) transaction_listener.onAckCltTerminated(this); 
-      // (CHANGE-040421) free the link to transaction_listener
-      transaction_listener=null;
-   }     
-         
-   /** Method used to drop an active transaction. */
-   public void terminate()
-   {  changeStatus(STATE_TERMINATED);  
-      // (CHANGE-040421) free the link to transaction_listener
-      transaction_listener=null;
-   }
-  
-
-   //**************************** Logs ****************************/
-
-   /** Adds a new string to the default Log */
-   protected void printLog(String str, int level)
-   {  if (log!=null) log.println("AckTransactionClient#"+transaction_sqn+": "+str,level+SipStack.LOG_LEVEL_TRANSACTION);  
-   }
-
+	/** Method used to drop an active transaction. */
+	public void terminate() {
+		changeStatus(STATE_TERMINATED);
+		// (CHANGE-040421) free the link to transaction_listener
+		transaction_listener = null;
+	}
 }
--- branches/red5sip/src/java/org/zoolu/sip/transaction/Transaction.java
+++ branches/red5sip/src/java/org/zoolu/sip/transaction/Transaction.java
@@ -23,171 +23,151 @@
 
 package org.zoolu.sip.transaction;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zoolu.sip.message.Message;
 import org.zoolu.sip.provider.ConnectionIdentifier;
 import org.zoolu.sip.provider.SipProvider;
 import org.zoolu.sip.provider.SipProviderListener;
-import org.zoolu.sip.provider.SipStack;
 import org.zoolu.sip.provider.TransactionIdentifier;
-import org.zoolu.tools.Log;
-import org.zoolu.tools.LogLevel;
 import org.zoolu.tools.Timer;
 import org.zoolu.tools.TimerListener;
 
-
-/** Abstract class Transaction is hinerited by classes
- *  ClientTransaction, ServerTransaction, InviteClientTransaction and InviteServerTransaction.
- *  An Object Transaction is responsable to handle a new SIP transaction.<BR>
- *  The changes of the internal status and the received messages are fired to the
- *  TransactionListener passed to the Transaction Objects.<BR>
+/**
+ * Abstract class Transaction is hinerited by classes ClientTransaction, ServerTransaction, InviteClientTransaction and
+ * InviteServerTransaction. An Object Transaction is responsable to handle a new SIP transaction.<BR>
+ * The changes of the internal status and the received messages are fired to the TransactionListener passed to the
+ * Transaction Objects.<BR>
  */
-public abstract class Transaction implements SipProviderListener, TimerListener
-{ 
-   /** Transactions counter */
-   protected static int transaction_counter=0;
+public abstract class Transaction implements SipProviderListener, TimerListener {
+	protected static Logger log = LoggerFactory.getLogger(Transaction.class);
+	/** Transactions counter */
+	protected static int transaction_counter = 0;
 
-   // all transaction states:
-   /** State Waiting, used only by server transactions */
-   static final int STATE_IDLE=0; 
-   /** State Waiting, used only by server transactions */
-   static final int STATE_WAITING=1; 
-   /** State Trying */
-   static final int STATE_TRYING=2; 
-   /** State Proceeding */
-   static final int STATE_PROCEEDING=3;
-   /** State Completed */
-   static final int STATE_COMPLETED=4;
-   /** State Confirmed, used only by invite server transactions */
-   static final int STATE_CONFIRMED=5;
-   /** State Waiting. */
-   static final int STATE_TERMINATED=7; 
-  
-   /** Gets the transaction state. */
-   static String getStatus(int st)
-   {  switch (st)
-      {  case STATE_IDLE       : return "T_Idle";
-         case STATE_WAITING    : return "T_Waiting"; 
-         case STATE_TRYING     : return "T_Trying"; 
-         case STATE_PROCEEDING : return "T_Proceeding";
-         case STATE_COMPLETED  : return "T_Completed";
-         case STATE_CONFIRMED  : return "T_Confirmed";
-         case STATE_TERMINATED : return "T_Terminated";
-         default : return null;
-      }
-   }
+	// all transaction states:
+	/** State Waiting, used only by server transactions */
+	static final int STATE_IDLE = 0;
+	/** State Waiting, used only by server transactions */
+	static final int STATE_WAITING = 1;
+	/** State Trying */
+	static final int STATE_TRYING = 2;
+	/** State Proceeding */
+	static final int STATE_PROCEEDING = 3;
+	/** State Completed */
+	static final int STATE_COMPLETED = 4;
+	/** State Confirmed, used only by invite server transactions */
+	static final int STATE_CONFIRMED = 5;
+	/** State Waiting. */
+	static final int STATE_TERMINATED = 7;
 
+	/** Gets the transaction state. */
+	static String getStatus(int st) {
+		switch (st) {
+			case STATE_IDLE:
+				return "T_Idle";
+			case STATE_WAITING:
+				return "T_Waiting";
+			case STATE_TRYING:
+				return "T_Trying";
+			case STATE_PROCEEDING:
+				return "T_Proceeding";
+			case STATE_COMPLETED:
+				return "T_Completed";
+			case STATE_CONFIRMED:
+				return "T_Confirmed";
+			case STATE_TERMINATED:
+				return "T_Terminated";
+			default:
+				return null;
+		}
+	}
 
-   /** Transaction sequence number */
-   int transaction_sqn;
+	/** Transaction sequence number */
+	int transaction_sqn;
 
-   /** Event logger. */
-   Log log;
- 
-   /** Lower layer dispatcher that sends and receive messages.
-     * The messages received by the SipProvider are fired to the Transaction
-     * by means of the onReceivedMessage() method. */
-   SipProvider sip_provider;
- 
-   /** Internal state-machine status */
-   int status;
+	/**
+	 * Lower layer dispatcher that sends and receive messages. The messages received by the SipProvider are fired to the
+	 * Transaction by means of the onReceivedMessage() method.
+	 */
+	SipProvider sip_provider;
 
-   /** transaction request message/method */
-   Message request;
-   
-   /** the Transaction ID */
-   TransactionIdentifier transaction_id;
+	/** Internal state-machine status */
+	int status;
 
-   /** Transaction connection id */
-   ConnectionIdentifier connection_id;
+	/** transaction request message/method */
+	Message request;
 
+	/** the Transaction ID */
+	TransactionIdentifier transaction_id;
 
-   /** Costructs a new Transaction */
-   protected Transaction(SipProvider sip_provider)
-   {  this.sip_provider=sip_provider;
-      log=sip_provider.getLog();
-      this.transaction_id=null;
-      this.request=null;
-      this.connection_id=null;
-      this.transaction_sqn=transaction_counter++;
-      this.status=STATE_IDLE;
-   }
+	/** Transaction connection id */
+	ConnectionIdentifier connection_id;
 
-   /** Changes the internal status */
-   void changeStatus(int newstatus)
-   {  status=newstatus;
-      //transaction_listener.onChangedTransactionStatus(status);
-      printLog("changed transaction state: "+getStatus(),LogLevel.MEDIUM);
-   }
-   
-   /** Whether the internal status is equal to <i>st</i> */
-   boolean statusIs(int st)
-   {  return status==st;
-   }      
+	/** Costructs a new Transaction */
+	protected Transaction(SipProvider sip_provider) {
+		this.sip_provider = sip_provider;
+		this.transaction_id = null;
+		this.request = null;
+		this.connection_id = null;
+		this.transaction_sqn = transaction_counter++;
+		this.status = STATE_IDLE;
+	}
 
-   /** Gets the current transaction state. */
-   String getStatus()
-   {  return getStatus(status);
-   }
+	/** Changes the internal status */
+	void changeStatus(int newstatus) {
+		status = newstatus;
+		// transaction_listener.onChangedTransactionStatus(status);
+		log.debug("changed transaction state: " + getStatus());
+	}
 
-   /** Gets the SipProvider of this Transaction. */
-   public SipProvider getSipProvider()
-   {  return sip_provider;
-   }
+	/** Whether the internal status is equal to <i>st</i> */
+	boolean statusIs(int st) {
+		return status == st;
+	}
 
-   /** Gets the Transaction request message */
-   public Message getRequestMessage()
-   {  return request;
-   }
+	/** Gets the current transaction state. */
+	String getStatus() {
+		return getStatus(status);
+	}
 
-   /** Gets the Transaction method */
-   public String getTransactionMethod()
-   {  return request.getTransactionMethod();
-   }
+	/** Gets the SipProvider of this Transaction. */
+	public SipProvider getSipProvider() {
+		return sip_provider;
+	}
 
-   /** Gets the transaction-ID */
-   public TransactionIdentifier getTransactionId()
-   {  return transaction_id;
-   }
+	/** Gets the Transaction request message */
+	public Message getRequestMessage() {
+		return request;
+	}
 
-   /** Gets the transaction connection id */
-   public ConnectionIdentifier getConnectionId()
-   {  return connection_id;
-   }
+	/** Gets the Transaction method */
+	public String getTransactionMethod() {
+		return request.getTransactionMethod();
+	}
 
-   /** Method derived from interface SipListener.
-     * It's fired from the SipProvider when a new message is catch for to the present ServerTransaction.
-     */
-   public void onReceivedMessage(SipProvider provider, Message msg)
-   {  //do nothing
-   }
-   
-   /** Method derived from interface TimerListener.
-     * It's fired from an active Timer.
-     */
-   public void onTimeout(Timer to)
-   {  //do nothing
-   }
+	/** Gets the transaction-ID */
+	public TransactionIdentifier getTransactionId() {
+		return transaction_id;
+	}
 
-   /** Terminates the transaction. */
-   public abstract void terminate();
-   
+	/** Gets the transaction connection id */
+	public ConnectionIdentifier getConnectionId() {
+		return connection_id;
+	}
 
-   //**************************** Logs ****************************/
+	/**
+	 * Method derived from interface SipListener. It's fired from the SipProvider when a new message is catch for to the
+	 * present ServerTransaction.
+	 */
+	public void onReceivedMessage(SipProvider provider, Message msg) { // do nothing
+	}
 
-   /** Adds a new string to the default Log */
-   protected void printLog(String str, int level)
-   {  if (log!=null) log.println("Transaction#"+transaction_sqn+": "+str,level+SipStack.LOG_LEVEL_TRANSACTION);  
-   }
+	/**
+	 * Method derived from interface TimerListener. It's fired from an active Timer.
+	 */
+	public void onTimeout(Timer to) { // do nothing
+	}
 
-   /** Adds a WARNING to the default Log */
-   protected void printWarning(String str, int level)
-   {  printLog("WARNING: "+str,level); 
-   }
-
-   /** Adds the Exception to the log file */
-   protected void printException(Exception e, int level)
-   {  if (log!=null) log.printException(e,level+SipStack.LOG_LEVEL_TRANSACTION);
-   }
-
+	/** Terminates the transaction. */
+	public abstract void terminate();
 }
